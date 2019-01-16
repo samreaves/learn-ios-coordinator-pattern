5ff71d0c35cb99f19d9dcff6d1ce570029d58120
Sam <thesamreaves@gmail.com>
2019-01-15 20:15:58 -0500

    Initial Commit

---
+++ CoordinatorTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorTest
+//
+//  Created by Sam Reaves on 1/15/19.
+//  Copyright © 2019 Sam Reaves Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
